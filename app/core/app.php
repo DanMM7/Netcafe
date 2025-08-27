ba4441522bc4df0cd13914c8ea85b1c05ae074d6
Dan HP <dmalengela@yahoo.com>
2025-08-27 00:01:28 -0500

    Refactor app.php to improve code clarity by adding comments and enhancing conditional checks for controller and method existence

--- app/core/app.php
+++ app/core/app.php
@@ -9,7 +9,8 @@
             
             $url = $this->parseUrl();
 
-            if(file_exists('../app/controllers/' . strtolower($url[0]) . '.php')) {
+            // Check if controller exists
+            if(isset($url[0]) && file_exists('../app/controllers/' . strtolower($url[0]) . '.php')) {
                 $this->controller = strtolower($url[0]);
                 unset($url[0]);
             }
@@ -17,22 +18,24 @@
             require_once '../app/controllers/' . $this->controller . '.php';
             $this->controller = new $this->controller;
 
-            if(isset($url[1])) {
-                if(method_exists($this->controller, $url[1])) {
-                    $this->method = $url[1];
-                    unset($url[1]);
-                }
+            // Check if method exists
+            if(isset($url[1]) && method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
             }
 
-            $this->params = (count($url) > 0) ? array_values($url) : ['home'];
+            // Get parameters
+            $this->params = $url ? array_values($url) : [];
 
+            // Call the method
             call_user_func_array([$this->controller, $this->method], $this->params);
 
         }
 
         private function parseUrl() {
             if(isset($_GET['url'])) {
-                return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+                return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
             }
+            return ['home']; // Return default controller if no URL is set
         }
     }
